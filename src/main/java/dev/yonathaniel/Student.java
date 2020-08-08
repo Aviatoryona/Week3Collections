70b460d014e129eda7d3f82f5c46100a3e5c98d3
Aviator Yonathaniel <aviatoryona67@gmail.com>
2020-08-08 10:29:30 +0300

    Output file

--- src/main/java/dev/yonathaniel/Student.java
+++ src/main/java/dev/yonathaniel/Student.java
@@ -1,9 +1,12 @@
 package dev.yonathaniel;
 
+import java.sql.SQLException;
+import java.util.Map;
+
 /*
    A class to create object for student details
  */
-public class Student {
+public class Student implements StudentI{
     private String name;
     private String admissionNumber;
 
@@ -27,5 +30,25 @@ public class Student {
                 "\n" +
                 "Admission Number: " + this.getAdmissionNumber() +
                 "\n";
+    }
+
+    @Override
+    public Map<Integer, Student> getStudents() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean addStudent(Student student) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean deleteStudent(int id) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean updateStudent(int id, Student student) throws SQLException {
+        return false;
     }
 }
